6812c5ab6ba7aff1c2bb2b63113b36996f377735
zaftie <zaftreal@gmail.com>
2024-01-26 14:14:31 -0800

    navbar when smaller window
    made it so items turn into collapsable
    made it so stays in place still need to fix it when it gets to really small numbers

--- portfolio/src/Components/NavScrollExample.js
+++ portfolio/src/Components/NavScrollExample.js
@@ -7,18 +7,20 @@ import '../Styling/NavBarStyle.css'
 
 function BasicExample() {
   return (
-    <Navbar  bg="dark" data-bs-theme="dark">
+    <Navbar  bg="dark" data-bs-theme="dark" expand="lg">
     <Container>
       <Navbar.Brand  href="#home" className="leftmost-navbar">Frankie's Port</Navbar.Brand>
-      <Nav className=" d-flex justify-content-center align-items-center">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      
+      <Nav className='me-auto'>
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#projects">Projects</Nav.Link>
         <Nav.Link href="#resume">Resume</Nav.Link>
         <Nav.Link href="#contact">Contact</Nav.Link>
         <Nav.Link href="#skills">Skills and Technology</Nav.Link>
-
-
       </Nav>
+    </Navbar.Collapse>
     </Container>
   </Navbar>
   );
